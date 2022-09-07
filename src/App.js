8882dbfffc9d61349ff2661bfebeb08697eb99ac
yash9650 <yash16022001@gmail.com>
2022-09-08 02:04:30 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(()=>{
+    fetch('/home',{
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }).then(res => res.json()).then((result) => {
+      console.log(result);
+    }).catch(err => {
+      console.log(err);
+    })
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Home/>
+    </>
   );
 }
 
